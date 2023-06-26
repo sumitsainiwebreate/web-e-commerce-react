fd28884fc0ff25831595fdd3c360d36444936d08
sumitsaini5767 <sumitsaini576@gmail.com>
2023-06-26 10:46:01 +0530

    .

---
+++ public/js/header.js
@@ -0,0 +1,19 @@
+$(function(){
+    // $("#header").load("D:\work\webreate\header.html");
+
+    $("#header").load("../../header.html");
+    $("#footer").load("../../footer.html");
+    $("#insta-area").load("../../social-media-sec.html");
+
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("stickynav").style.top = "0";
+  } else {
+    document.getElementById("stickynav").style.top = "-100%";
+  }
+  prevScrollpos = currentScrollPos;
+}
